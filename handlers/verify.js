047536c5da11e19538013f630a3406cb6f29f814
ModsXtools <ibrahim8shahz10@gmail.com>
2026-06-30 20:07:12 +0500

    Delete handlers/verify.js

--- handlers/verify.js
+++
@@ -1,9 +0,0 @@
-import { sendMessage } from "../services/telegram.js";
-
-export async function verifyJoin(env, callback) {
-  await sendMessage(
-    env,
-    callback.message.chat.id,
-    "✅ Verification feature will be added next."
-  );
-}
