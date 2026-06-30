5f5fb836d8202670c25ce58104e6a8dcbdaf59c4
ModsXtools <ibrahim8shahz10@gmail.com>
2026-06-30 11:31:51 +0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-export function getConfig(env) {
-  return {
-    BOT_TOKEN: env.BOT_TOKEN,
-    SUPABASE_URL: env.SUPABASE_URL,
-    SUPABASE_SECRET_KEY: env.SUPABASE_SECRET_KEY,
-    ADMIN_ID: env.ADMIN_ID
-  };
-}
