83b2d160ea503c397da19b338eb98f37c2a5c10c
ModsXtools <ibrahim8shahz10@gmail.com>
2026-07-01 10:10:17 +0500

    Refactor user functions and add is_verified field
    Refactor user creation and retrieval logic, add is_verified field, and improve null handling.

--- models/user.js
+++ models/user.js
@@ -10,31 +10,49 @@ export async function getUser(env, telegramId) {
     `?telegram_id=eq.${telegramId}`
   );
 
-  return users.length ? users[0] : null;
+  return users?.[0] || null;
 }
 
 export async function createUser(env, user) {
-  return await query(
-    env,
-    "users",
-    "POST",
-    {
-      telegram_id: user.id,
-      username: user.username || "",
-      first_name: user.first_name || "",
-      credits: 0,
-      referral_code: generateCode(user.id)
-    }
-  );
+  await query(env, "users", "POST", {
+    telegram_id: user.id,
+    username: user.username || "",
+    first_name: user.first_name || "",
+    credits: 0,
+    is_verified: false,
+    referral_code: generateCode(user.id)
+  });
+
+  return await getUser(env, user.id);
 }
 
 export async function getOrCreateUser(env, telegramUser) {
   let user = await getUser(env, telegramUser.id);
 
   if (!user) {
-    await createUser(env, telegramUser);
-    user = await getUser(env, telegramUser.id);
+    user = await createUser(env, telegramUser);
   }
 
   return user;
+}
+
+export async function getCredits(env, telegramId) {
+  const user = await getUser(env, telegramId);
+  return user?.credits || 0;
+}
+
+export async function addCredits(env, telegramId, amount) {
+  const current = await getCredits(env, telegramId);
+
+  await query(
+    env,
+    "users",
+    "PATCH",
+    {
+      credits: current + amount
+    },
+    `?telegram_id=eq.${telegramId}`
+  );
+
+  return current + amount;
 }
